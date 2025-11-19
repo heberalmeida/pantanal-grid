9964421a25e0c102f802c711ddea6856ae406130
Heber Almeida <eu@heber.com.br>
2025-11-19 16:02:40 -0400

    fix: mark internal page updates to prevent watch loops
    - Set isInternalPageUpdate and isInternalPageSizeUpdate flags in handlers
    - Prevents watch from blocking updates when Pagination emits events

--- packages/pantanal-grid/src/components/Grid.vue
+++ packages/pantanal-grid/src/components/Grid.vue
@@ -1470,8 +1470,8 @@
           <template v-if="props.pageableSlots?.buttons" #buttons="slotProps">
             <slot :name="props.pageableSlots.buttons" v-bind="slotProps" />
           </template>
-          @update:page="(p: number) => page.value = p"
-          @update:pageSize="(s: number) => pageSize.value = s"
+          @update:page="(p: number) => { isInternalPageUpdate = true; page.value = p }"
+          @update:pageSize="(s: number) => { isInternalPageSizeUpdate = true; pageSize.value = s }"
           @refresh="() => emit('refresh')" 
         </GridPagination>
       </div>
